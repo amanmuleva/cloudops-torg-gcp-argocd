4b6c59dc77359177711fe7f15f75044daf0ede92
amanmuleva <aman.muleva@vonage.com>
2025-09-22 11:31:42 +0530

    Create env-setup.py

---
+++ .github/workflows/env-setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import yaml
+
+def dir_to_dict(path):
+    directory = {}
+    dn = os.path.basename(path)
+    subdirs = []
+
+    try:
+        for d in os.listdir(path):
+            full_path = os.path.join(path, d)
+            if os.path.isdir(full_path) and d not in ['root-app', 'templates', 'ca-gcf']:
+                subdirs.append(dir_to_dict(full_path))
+    except Exception as e:
+        print(f"Error reading {path}: {e}")
+    if subdirs:
+        return {dn: subdirs}
+    else:
+        return dn
+
+if len(sys.argv) == 1:
+    p = os.getcwd()
+elif len(sys.argv) == 2:
+    p = os.path.abspath(sys.argv[1])
+else:
+    sys.stderr.write("Unexpected argument {}\n".format(sys.argv[2:]))
+
+try:
+    with open(f"{os.path.basename(p)}.yaml", "w") as f:
+        try:
+            yaml.dump(dir_to_dict(path=p), f, default_flow_style=False)
+            print(f"Directory structure written to {os.path.basename(p)}.yaml")
+        except Exception as e:
+            print(e)
+except Exception as e:
+    print(e)
